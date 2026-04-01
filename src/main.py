cb75dbbc5df13dc0ebb6129187f2f5300ebab4d5
Li-Thao-Phung <lphung23@student.aau.dk>
2026-04-01 15:35:40 +0200

    pipline finpusede

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from Discretization.discretizationSetup import (
 )
 from Discretization.naive import equal_width_discretization
 
-from dataProcessing.processData import (
+from DataProcessing.processData import (
     format_temperature_data,
     extract_time_intervals
 )
@@ -31,11 +31,11 @@ from dataProcessing.processData import (
 
 
 #Prepare Data for TAG
-k = 3
+k = 12
 input_files = [
     #'DataProcessing/formated_data.csv',
     #'DataProcessing/formated_data2.csv'
-    '../Data/4-ExtractInterval/experiment_1_full_days/trace_trace1.csv'
+    '../Data/4-ExtractInterval/experiment_3_weekly/trace_trace1.csv'
 ]
 
 data_lists = csv_to_temp_time_list(input_files)
@@ -61,7 +61,7 @@ format_output(symbolic_trace, tss_path)
 
 # Call TAG
 #tss_path = 'Discretization/output.txt'
-#xml_path = '../Data/7-XMLOutput/model.xml'
+#xml_path = '../Data/7-XMLOutput/model-5h.xml'
 
 # with open('Discretization/symbol_map.json') as f:
 #     symbol_map = json.load(f)
@@ -74,9 +74,7 @@ TA_output_path = os.path.join("../Data", "6-TaResults")
 learner.ta.show(title = title, savePng = True, output_path = TA_output_path)
 
 
-os.path.join("../Data", "7-XMLOutput", "model.xml")
-
-XML_output_path = os.path.join("../Data", "7-XMLOutput", "model.xml")
+XML_output_path = os.path.join("../Data", "7-XMLOutput", "model-1week-k12.xml")
 learner.ta.export_ta(path=XML_output_path, symbol_map=symbol_map)
 print(f"UPPAAL model written to {XML_output_path}")
 
